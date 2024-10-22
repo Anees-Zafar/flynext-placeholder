ce636e7722cbcfd64d03827fc45655f7bf73f856
Anees-Zafar <aneesdeveloper038@gmail.com>
2024-10-22 17:28:55 +0500

    fotter globe repair

--- assets/js/main.js
+++ assets/js/main.js
@@ -627,3 +627,48 @@ $('.ratings-form-text').on('click', function(){
                 }, 3000);
             });
     });
+
+
+
+
+    const form2 = document.getElementById('form2');
+    const result2 = document.getElementById('formresult2');
+    const submitbtn2 = document.getElementById('formsubmitbtn2');
+    
+    form2.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const formData = new FormData(form2);
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+      result2.innerHTML = "Please wait..."
+      submitbtn2.style.display = 'none'; 
+        fetch('https://api.web3forms.com/submit', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: json
+            })
+            .then(async (response) => {
+                let json = await response.json();
+                if (response.status == 200) {
+                    result2.innerHTML = json.message;
+
+
+                } else {
+                    console.log(response);
+                    result2.innerHTML = json.message;
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                result2.innerHTML = "Something went wrong!";
+            })
+            .then(function() {
+                form2.reset();
+                setTimeout(() => {
+                    result2.style.display = "none";
+                }, 3000);
+            });
+    });
